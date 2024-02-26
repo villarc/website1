41d50c9ac203d1d97ed6eddfef2d86c305672155
villarc <142903187+villarc@users.noreply.github.com>
2024-02-26 05:30:49 +0000

    Sun, Feb 25, 2024, 9:30 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -8,6 +8,10 @@ int = setInterval(blurring,30)
 
 function blurring() {
     load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = '${load}%'
     loadText.style.opacity = 1
     bg.style.filter = "blur(10px)"
