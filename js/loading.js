1d5c760416d7de66807cdae1fc95e81657e1cd82
villarc <142903187+villarc@users.noreply.github.com>
2024-02-26 05:25:11 +0000

    Sun, Feb 25, 2024, 9:25 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,8 @@
 loadText = document.querySelector('loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
 loadText.innerText = '${load}%'
 loadText.style.opacity = 1
+bg.style.filter = "blur(10px)"
