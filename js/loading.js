e27cf6ac2295cbe30e3e2ee3b9e5458a927dc905
villarc <142903187+villarc@users.noreply.github.com>
2024-02-26 05:36:56 +0000

    Sun, Feb 25, 2024, 9:36 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -11,8 +11,9 @@ function blurring() {
 
     if (load > 99){
         clearInterval(int)
-    }
-    loadText.innerText = '${load}%'
-    loadText.style.opacity = 1
-    bg.style.filter = "blur(10px)"
+}
+loadText.innerText = '${load}%'
+loadText.style.opacity = 1 - load/100
+
+bg.style.filter = 'blur($(30-(load/100)*30)px)
 }
